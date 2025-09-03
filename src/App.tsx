0cebacf2f38c9431b1cd0fb0498981d215f6a66c
wjdgotjd28-del <wjdgotjd28@gmail.com>
2025-09-03 17:56:00 +0900

    9/3

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AppBar,
+  Box,
+  Container,
+  CssBaseline,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+// import "./App.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxWidth="xl">
+        <CssBaseline />
+        <AppBar position="static" sx={{ width: "100%" }}>
+          <Toolbar>
+            <Typography variant="h6">냥냥</Typography>
+            <Box
+              sx={{ flexGrow: 0.5, display: "flex", justifyContent: "center" }}
+            ></Box>
+            <TextField
+              // value={searchQuery}
+              // onChange={handleSearchChange}
+              variant="outlined"
+              size="small"
+              placeholder="Search..."
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton edge="start">
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              sx={{ ml: "auto" }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        {/* <Routes>
+          <Route path="/" element={<PrivateRoute><CarList/></PrivateRoute>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes> */}
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
